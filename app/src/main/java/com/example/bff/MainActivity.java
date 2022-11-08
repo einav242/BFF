108a6cf199bbc4d7a92937f96c0771d3c7c82112
e2402 <e24022997b@gmail.com>
2022-11-08 18:02:18 +0200

    version 2

--- app/src/main/java/com/example/bff/MainActivity.java
+++ app/src/main/java/com/example/bff/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view){
+        TextView txtFirstName = findViewById(R.id.textView2);
+        EditText edtTxtName = findViewById(R.id.editTextName);
+        txtFirstName.setText(edtTxtName.getText().toString());
+
+        TextView txtLastName = findViewById(R.id.textView3);
+        EditText edtTxtName2 = findViewById(R.id.edtTxt2);
+        txtLastName.setText(edtTxtName2.getText().toString());
+
+        TextView txtEmail = findViewById(R.id.textView4);
+        EditText edtTxtName3 = findViewById(R.id.editTxt3);
+        txtEmail.setText(edtTxtName3.getText().toString());
     }
 }
