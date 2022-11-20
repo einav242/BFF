5eca913e273cb1f566642766bf9caa13b183d777
nivmoti <nivnivnivniv00@gmail.com>
2022-11-20 19:40:58 +0200

    sync with the firebase

--- app/src/main/java/com/example/bff/MainActivity.java
+++ app/src/main/java/com/example/bff/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.bff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Button signup;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup = findViewById(R.id.button3);
+        login = findViewById(R.id.button2);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this , Register.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
+}
 
 //    public void onBtnClick(View view){
 //        TextView txtFirstName = findViewById(R.id.textView2);
@@ -28,4 +50,3 @@ public class MainActivity extends AppCompatActivity {
 //        EditText edtTxtName3 = findViewById(R.id.editTxt3);
 //        txtEmail.setText(edtTxtName3.getText().toString());
 //    }
-}
