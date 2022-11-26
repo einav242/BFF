a295f53beacde9b82260fc9f724382fe4c332738
bar87 <bar8870@gmail.com>
2022-11-26 19:19:58 +0200

    user can update is profile

---
+++ app/src/main/java/com/example/bff/searchWinForVet.java
@@ -0,0 +1,14 @@
+package com.example.bff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchWinForVet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_win_for_vet);
+    }
+}
