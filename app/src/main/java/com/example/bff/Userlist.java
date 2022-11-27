401a8d02d965bbe37f1d1dd84282c94536c668b6
nivmoti <nivnivnivniv00@gmail.com>
2022-11-27 15:44:02 +0200

    make data for bussnies

--- app/src/main/java/com/example/bff/Userlist.java
+++ app/src/main/java/com/example/bff/Userlist.java
@@ -14,13 +14,11 @@ import java.util.Currency;
 public class Userlist extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<String> name, email, age;
-    DBHelper DB;
     MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
-        DB = new DBHelper(this);
         name = new ArrayList<>();
         email = new ArrayList<>();
         age = new ArrayList<>();
@@ -28,25 +26,7 @@ public class Userlist extends AppCompatActivity {
         adapter = new MyAdapter(this, name, email, age);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        displaydata();
     }
 
-    private void displaydata()
-    {
-        Cursor cursor = DB.getdata();
-        if(cursor.getCount()==0)
-        {
-            Toast.makeText(Userlist.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        else
-        {
-            while(cursor.moveToNext())
-            {
-                name.add(cursor.getString(0));
-                email.add(cursor.getString(1));
-                age.add(cursor.getString(2));
-            }
-        }
-    }
+
 }
--- app/src/main/java/com/example/bff/deleteUser.java
+++ app/src/main/java/com/example/bff/deleteUser.java
@@ -17,27 +17,14 @@ public class deleteUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.remove_client);
-        name = findViewById(R.id.editTextTextPersonName);
         email = findViewById(R.id.editTextTextEmailAddress);
-        id = findViewById(R.id.editTextTextPersonName3);
         delete = findViewById(R.id.button5);
-        DB = new DBHelper(this);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String nameTXT = name.getText().toString();
 
-                Boolean checkdeletedata  = DB.deleteuserdata(nameTXT);
-                if(checkdeletedata==true)
-                {
-                    Toast.makeText(deleteUser.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(deleteUser.this, "Entry Not Deleted", Toast.LENGTH_SHORT).show();
-                }
-                startActivity(new Intent(deleteUser.this, businessActivity.class));
-            }
+               }
         });
     }
 }
