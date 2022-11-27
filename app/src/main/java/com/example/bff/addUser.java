bfad29ecfaf27a955f80ab3bfcc6085e307260af
nivmoti <nivnivnivniv00@gmail.com>
2022-11-27 15:26:42 +0200

    make data for bussnies

--- app/src/main/java/com/example/bff/addUser.java
+++ app/src/main/java/com/example/bff/addUser.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 public class addUser extends AppCompatActivity {
     EditText name, email, phone, animalname, date;
     Button add;
-    DBHelper DB;
     private DatabaseReference mRootRef;
     private FirebaseAuth mAuth;
     ProgressDialog pd;
@@ -43,6 +42,9 @@ public class addUser extends AppCompatActivity {
         setContentView(R.layout.insert_client);
         name = findViewById(R.id.editTextTextPersonName);
         email = findViewById(R.id.editTextTextEmailAddress);
+        phone = findViewById(R.id.editTextPhone);
+        animalname = findViewById(R.id.editTextTextPersonName2);
+        date = findViewById(R.id.editTextDate);
         add =findViewById(R.id.button5);
         mRootRef = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
@@ -52,14 +54,39 @@ public class addUser extends AppCompatActivity {
             public void onClick(View v) {
                 String txtName = name.getText().toString();
                 String txtEmail = email.getText().toString();
+                String txtphone = phone.getText().toString();
+                String txtAnimalName = animalname.getText().toString();
+                String txtDate = date.getText().toString();
 
-                if (TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtName)
-                        || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
-                    Toast.makeText(Register.this, "Empty credentials!", Toast.LENGTH_SHORT).show();
-                } else if (txtPassword.length() < 6){
-                    Toast.makeText(Register.this, "Password too short!", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(txtName)
+                        || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtphone)|| TextUtils.isEmpty(txtAnimalName) || TextUtils.isEmpty(txtDate)){
+                    Toast.makeText(addUser.this, "Empty credentials!", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(txtUsername , txtName , txtEmail , txtPassword);
+                    addUsersD(txtName , txtEmail , txtphone, txtAnimalName,txtDate);
+                }
+            }
+        });
+    }
+    private void addUsersD(final String name, final String email, String phone,String animalName,String date){
+        pd.setMessage("Please Wait!");
+        pd.show();
+        HashMap<String , Object> map = new HashMap<>();
+        map.put("bussniessemail" , mAuth.getCurrentUser().getEmail());
+        map.put("email", email);
+//        map.put("phone" , phone);
+//        map.put("animalName", animalName);
+//        map.put("Date",date);
+        mRootRef.child("Em").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    pd.dismiss();
+                    Toast.makeText(addUser.this, "Update the profile " +
+                            "for better expereince", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(addUser.this , MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
