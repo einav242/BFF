ceb8e86ad89b2c7f1bfffe2955cac94b6855a606
nivmoti <nivnivnivniv00@gmail.com>
2022-11-27 16:38:52 +0200

    login mainactivitys

--- app/src/main/java/com/example/bff/MainActivity.java
+++ app/src/main/java/com/example/bff/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.bff;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private Button singupBusiness;
     private Button forget;
-    String permit="empty";
+    String permit = "empty";
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,83 +37,105 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.button2);
         singupBusiness = findViewById(R.id.button4);
         forget = findViewById(R.id.button);
+        EditText user_name = findViewById(R.id.editTextName);
+        EditText password = findViewById(R.id.edtTxt2);
+        mAuth = FirebaseAuth.getInstance();
 
 
         singupAnimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this , Register.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                startActivity(new Intent(MainActivity.this, Register.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
             }
         });
         singupBusiness.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this , register_BusinessOwner.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                startActivity(new Intent(MainActivity.this, register_BusinessOwner.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
             }
         });
         forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("hi");
-                startActivity(new Intent(MainActivity.this , forget_password.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                startActivity(new Intent(MainActivity.this, forget_password.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
             }
         });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText user_name = findViewById(R.id.editTextName);
-                EditText password = findViewById(R.id.edtTxt2);
-                if(MainActivity.this.permit=="empty")
-                {
+                String username = user_name.getText().toString();
+                String passwordE = password.getText().toString();
+                if (MainActivity.this.permit == "empty") {
                     TextView txtFirstName = findViewById(R.id.textView4);
                     txtFirstName.setText("Please select a permission");
                     txtFirstName.setTextColor(Color.RED);
                 }
-                if (user_name.getText().toString().equals("") ||  password.getText().toString().equals("")) {
-                    if (user_name.getText().toString().equals(""))
-                    {
-                        user_name.setError("Please Enter user name ");
-                    }
-                    if(password.getText().toString().equals(""))
-                    {
-                        password.setError("Please Enter password");
-                    }
-                    return;
-                }
-                if(MainActivity.this.permit=="animal")
-                {
-                    startActivity(new Intent(MainActivity.this, animalActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    finish();
-                }
-                else if(MainActivity.this.permit=="business")
-                {
-                    startActivity(new Intent(MainActivity.this, businessActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    finish();
-                }
-                else
-                {
-                    TextView txtFirstName = findViewById(R.id.textView4);
-                    txtFirstName.setText("Please select a permission");
-                }
+                if (TextUtils.isEmpty(username)) {
+                    user_name.setError("Email cannot be empty");
+                    user_name.requestFocus();
+                } else if (TextUtils.isEmpty(passwordE)) {
+                    password.setError("Password cannot be empty");
+                    password.requestFocus();
+                } else {
+                    mAuth.signInWithEmailAndPassword(username, passwordE).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                if (MainActivity.this.permit == "animal") {
+                                    Toast.makeText(MainActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(MainActivity.this, animalActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                                    finish();
+                                } else if (MainActivity.this.permit == "business") {
+                                    Toast.makeText(MainActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(MainActivity.this, businessActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                                    finish();
 
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
 
+//
+//
+                    });
+                }
             }
         });
     }
-    public void onRadioButtonClicked(View view) {
+    public void onRadioButtonClicked (View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.animal:
                 if (checked)
-                    this.permit="animal";
+                    this.permit = "animal";
                 break;
             case R.id.business:
                 if (checked)
-                    this.permit="business";
+                    this.permit = "business";
                 break;
         }
     }
 }
+
+//                    public void onRadioButtonClicked (View view){
+//                        // Is the button now checked?
+//                        boolean checked = ((RadioButton) view).isChecked();
+//
+//                        // Check which radio button was clicked
+//                        switch (view.getId()) {
+//                            case R.id.animal:
+//                                if (checked)
+//                                    this.permit = "animal";
+//                                break;
+//                            case R.id.business:
+//                                if (checked)
+//                                    this.permit = "business";
+//                                break;
+
+
+
