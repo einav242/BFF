7e42ac8400353dc6aac5c5ab81d9046ab8b18696
e2402 <e24022997b@gmail.com>
2022-11-29 13:27:05 +0200

    clean files

--- app/src/main/java/com/example/bff/Userlist.java
+++ app/src/main/java/com/example/bff/Userlist.java
@@ -12,20 +12,10 @@ import java.util.ArrayList;
 import java.util.Currency;
 
 public class Userlist extends AppCompatActivity {
-    RecyclerView recyclerView;
-    ArrayList<String> name, email, age;
-    MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
-        name = new ArrayList<>();
-        email = new ArrayList<>();
-        age = new ArrayList<>();
-        recyclerView = findViewById(R.id.recyclerview);
-        adapter = new MyAdapter(this, name, email, age);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
