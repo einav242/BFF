e6830f8738749c31411eecd56eac08c534d4198d
bar87 <bar8870@gmail.com>
2022-11-30 20:50:58 +0200

    add image for User 1.01

--- app/src/main/java/com/example/bff/addImage.java
+++ app/src/main/java/com/example/bff/addImage.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -15,11 +16,15 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.net.URI;
 import java.util.UUID;
 
 public class addImage extends AppCompatActivity {
@@ -30,6 +35,9 @@ public class addImage extends AppCompatActivity {
     private FirebaseStorage storage;
     private StorageReference storageReference;
 
+    private DatabaseReference mRootRef;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,54 +47,62 @@ public class addImage extends AppCompatActivity {
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
+        StorageReference profileRef = storageReference.child("user/"+ mAuth.getCurrentUser().getUid() +"profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profilePic);
+            }
+        });
+
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                choosePicture();
+                //open gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI) ;
+                startActivityForResult(openGalleryIntent,1000);
             }
         });
-    }
-
-    private void choosePicture() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+        if (requestCode==1000 && resultCode==RESULT_OK ){
             imageUri = data.getData();
-            profilePic.setImageURI(imageUri);
-            uploadPicture();
+            //profilePic.setImageURI(imageUri);
+            uploadPicture(imageUri);
         }
     }
 
-    private void uploadPicture() {
-
+    private void uploadPicture(Uri imageUri) {
+        //uplaod image to firebase storage
         final ProgressDialog pd = new ProgressDialog(this);
         pd.setTitle("Uploading Image...");
         pd.show();
 
 
         final  String randomKey = UUID.randomUUID().toString();
-        StorageReference riversRef = storageReference.child("images/" + randomKey);
 
+
+        StorageReference riversRef = storageReference.child("user/"+ mAuth.getCurrentUser().getUid() +"profile.jpg");
         // Register observers to listen for when the download is done or if it fails
         riversRef.putFile(imageUri).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
                 pd.dismiss();
-                Toast.makeText(getApplicationContext(), "Failed To Upload", Toast.LENGTH_SHORT).show();
+                Toast.makeText(addImage.this, "Failed To Upload", Toast.LENGTH_SHORT).show();
             }
         }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 pd.dismiss();
-//                Snackbar.make(findViewById(androidx.appcompat.R.id.content), "Image Uploaded." , Snackbar.LENGTH_LONG).show();
-                Snackbar.make(findViewById(R.id.edit_user_image), "Image Uploaded." , Snackbar.LENGTH_LONG).show();
+               riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                   @Override
+                   public void onSuccess(Uri uri) {
+                       Picasso.get().load(uri).into(profilePic);
+                   }
+               });
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
             @Override
