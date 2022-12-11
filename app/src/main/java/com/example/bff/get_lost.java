931d49995d8f67595852d796532549ae5b7bc5a8
bar87 <bar8870@gmail.com>
2022-12-11 18:49:34 +0200

    add user for Database when your dog getLost

--- app/src/main/java/com/example/bff/get_lost.java
+++ app/src/main/java/com/example/bff/get_lost.java
@@ -27,10 +27,12 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
-public class get_lost extends AppCompatActivity {
+public class get_lost extends AppCompatActivity
+{
 
     private FirebaseAuth mAuth;
     private Button addLost;
+    FirebaseUser userF;
 
     DataSnapshot dataSnapshot;
     DatabaseReference mRootRef;
@@ -43,30 +45,63 @@ public class get_lost extends AppCompatActivity {
         setContentView(R.layout.activity_get_lost);
 
         mRootRef = FirebaseDatabase.getInstance().getReference();
-        String email = mRootRef.child("email").toString();
-        String username = mRootRef.child("username").toString();
+//        String email = mRootRef.child("Users").child("email").toString();
+//        //String username = mRootRef.child("username").toString();
 
         addLost = findViewById(R.id.get_lost_IgotLost);
         mAuth = FirebaseAuth.getInstance();
 
+        userF = FirebaseAuth.getInstance().getCurrentUser();
 
         addLost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InserData(username,email);
-                finish();
+                InserData(userF.getUid(),userF.getEmail());
             }
         });
     }
 
+//    private void InserData(String username, String email) {
+//        String id = mRootRef.push().getKey();
+//        mRootRef.child("Got Lost").child(id).setValue(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()) {
+//                    Toast.makeText(get_lost.this, "User Details Inserted", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                pd.dismiss();
+//                Toast.makeText(get_lost.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+
     private void InserData(String username, String email) {
-        String id = mRootRef.push().getKey();
-        mRootRef.child("Got Lost").child(id).setValue(username).addOnCompleteListener(new OnCompleteListener<Void>() {
+        mAuth.createUserWithEmailAndPassword(email, username).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(get_lost.this, "User Derails Inserted", Toast.LENGTH_SHORT).show();
-                }
+            public void onSuccess(AuthResult authResult) {
+
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("username", username);
+                map.put("email", email);
+                map.put("id", mAuth.getCurrentUser().getUid());
+                mRootRef.child("Got Lost").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            pd.dismiss();
+                            Toast.makeText(get_lost.this, "User Details Inserted", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(get_lost.this, animalActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -76,6 +111,5 @@ public class get_lost extends AppCompatActivity {
             }
         });
     }
-
 
 }
