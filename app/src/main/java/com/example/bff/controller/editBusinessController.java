cde6a672725437e918fec410cbe07914bf1484c3
bar87 <bar8870@gmail.com>
2022-12-18 13:13:45 +0200

    MVC - Register

--- app/src/main/java/com/example/bff/controller/editBusinessController.java
+++ app/src/main/java/com/example/bff/controller/editBusinessController.java
@@ -1,36 +1,54 @@
 package com.example.bff.controller;
 
-import android.content.Context;
 import android.net.Uri;
-import android.widget.ImageView;
 
-import com.example.bff.entities.User;
-import com.example.bff.model.EditUserProfileModel;
 import com.example.bff.model.editBusinessModel;
 import com.example.bff.view.editBusinessView;
+
 
 public class editBusinessController {
 
     editBusinessModel model;
     editBusinessView view;
+
+
     public editBusinessController(editBusinessView view) {
         this.view = view;
         model = new editBusinessModel(this);
+    }
+
+    public void setDataController(String email ,String name, String businessName, String id ,String phone, String city,String street,
+                                  String house_number, String type , String time) {
+        view.setDataView(email,name, businessName, id, phone, city, street, house_number, type, time);
+    }
+
+    public void getDataController(){
+        model.getDataModel();
     }
 
     public void getImageController(){
         model.getImageModel();
     }
 
-    public void update(String newName,String newBusinessName, String newId, String newPhone, String newCity, String newStreet, String newHouseNumber, String newType, String newTime) {
-        model.update(newName,newBusinessName,newId,newPhone,newCity,newStreet,newHouseNumber,newType,newTime);
+    public void setImageController(Uri uri) {
+        view.setImegeView(uri);
     }
 
-    public void EditProfileimage_controller(ImageView profilePic) {
-        model.EditProfileimage_controller(profilePic);
+    public void updateController(String name, String businessName, String id ,String phone, String city,String street,
+                        String house_number, String type , String time) {
+        model.updateModel(name,businessName,id,phone,city, street, house_number, type, time);
     }
 
-    public void uploadPicture_controller(Uri imageUri) {
+    public void uploadPictureController(Uri imageUri) {
         model.uploadPicture_model(imageUri);
+    }
+    public void setToastController(String msg){
+        view.setToastView(msg);
+    }
+    public void setPdController(String msg){
+        view.setPdView(msg);
+    }
+    public void pdDismissController(){
+        view.pdDismissView();
     }
 }
--- app/src/main/java/com/example/bff/view/editBusinessView.java
+++ app/src/main/java/com/example/bff/view/editBusinessView.java
@@ -1,99 +1,127 @@
-//package com.example.bff.view;
-//
-//import android.content.Intent;
-//import android.net.Uri;
-//import android.os.Bundle;
-//import android.provider.MediaStore;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.annotation.Nullable;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.example.bff.R;
-//import com.example.bff.entities.User;
-//import com.example.bff.businessActivity;
-//import com.example.bff.controller.editBusinessController;
-//
-//public class editBusinessView extends AppCompatActivity {
-//    private EditText name;
-//    private EditText businessName;
-//    private TextView email;
-//    private EditText id;
-//    private EditText phone;
-//    private EditText city;
-//    private EditText street;
-//    private EditText house_number;
-//    private EditText type;
-//    private EditText time;
-//    private Button update;
-//    private User user;
-//
-//    private ImageView profilePic;
-//    public Uri imageUri;
-//    editBusinessController controller;
-//
-//
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.edit_business_activity);
-//
-//
-//        controller = new editBusinessController(this);
-//
-//
-//        name = findViewById(R.id.register_BO_fullName);
-//        businessName = findViewById(R.id.register_BO_BusinessName);
-//        email = findViewById(R.id.register_BO_Email);
-//        id = findViewById(R.id.register_BO_ID);
-//        phone = findViewById(R.id.register_BO_PhoneNumber);
-//        city = findViewById(R.id.register_BO_City);
-//        street = findViewById(R.id.register_BO_Street);
-//        house_number = findViewById(R.id.register_BO_StreetNum);
-//        type = findViewById(R.id.register_BO_TypeOfBusiness);
-//        time = findViewById(R.id.editTextTextPersonName2);
-//        update = findViewById(R.id.register_BO_Register);
-//        profilePic = findViewById(R.id.register_BO_image);
-//        controller.EditProfileimage_controller(profilePic);
-//
-//        controller.getImageController();
-//        controller.getDataController();
-//
-//        update.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                controller.update(name.getText().toString(),businessName.getText().toString(),id.getText().toString(),phone.getText().toString(),city.getText().toString(),street.getText().toString(),
-//                        house_number.getText().toString(),type.getText().toString(), time.getText().toString());
-//                startActivity(new Intent(editBusinessView.this, businessActivity.class));
-//            }
-//        });
-//
-//        //for add Images
-//        profilePic.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //open gallery
-//                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI) ;
-//                startActivityForResult(openGalleryIntent,1000);
-//            }
-//        });
-//
-//
-//    }
-//
-//
-//    //for add Image
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode==1000 && resultCode==RESULT_OK ){
-//            imageUri = data.getData();
-//            //profilePic.setImageURI(imageUri);
-//            controller.uploadPicture_controller(imageUri);
-//        }
-//    }
-//
-//}
+package com.example.bff.view;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bff.R;
+import com.example.bff.controller.editBusinessController;
+import com.example.bff.entities.Business;
+import com.squareup.picasso.Picasso;
+
+public class editBusinessView extends AppCompatActivity {
+
+    private EditText name;
+    private EditText businessName;
+    private TextView email;
+    private EditText id;
+    private EditText phone;
+    private EditText city;
+    private EditText street;
+    private EditText house_number;
+    private EditText type;
+    private EditText time;
+    private Button update;
+    editBusinessController controller;
+    ProgressDialog pd;
+    private ImageView profilePic;
+    public Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_business_activity);
+
+        //user can change
+        name = findViewById(R.id.register_BO_fullName);
+        businessName = findViewById(R.id.register_BO_BusinessName);
+        email = findViewById(R.id.register_BO_Email);
+        id = findViewById(R.id.register_BO_ID);
+        phone = findViewById(R.id.register_BO_PhoneNumber);
+        city = findViewById(R.id.register_BO_City);
+        street = findViewById(R.id.register_BO_Street);
+        house_number = findViewById(R.id.register_BO_StreetNum);
+        type = findViewById(R.id.register_BO_TypeOfBusiness);
+        time = findViewById(R.id.editTextTextPersonName2);
+        update = findViewById(R.id.register_BO_Register);
+        profilePic = findViewById(R.id.register_BO_image);
+        pd = new ProgressDialog(this);
+        controller = new editBusinessController(this);
+        controller.getImageController();
+        controller.getDataController();
+        //for add Images
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI) ;
+                startActivityForResult(openGalleryIntent,1000);
+            }
+        });
+
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                controller.updateController(name.getText().toString(),businessName.getText().toString(),id.getText().toString(),phone.getText().toString(),city.getText().toString(),street.getText().toString(),
+                        house_number.getText().toString(),type.getText().toString(), time.getText().toString());
+                startActivity(new Intent(editBusinessView.this, businessActivityView.class));
+            }
+        });
+    }
+
+
+    //for add Image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1000 && resultCode==RESULT_OK ){
+            imageUri = data.getData();
+            //profilePic.setImageURI(imageUri);
+            controller.uploadPictureController(imageUri);
+        }
+    }
+
+
+
+
+    public void setDataView(String email ,String name, String businessName, String id ,String phone, String city,String street,
+                            String house_number, String type , String time) {
+        this.name.setText(name);
+        this.businessName.setText(businessName);
+        this.email.setText(email);
+        this.id.setText(id);
+        this.phone.setText(phone);
+        this.city.setText(city);
+        this.street.setText(street);
+        this.house_number.setText(house_number);
+        this.type.setText(type);
+        this.time.setText(time);
+    }
+
+    public void setImegeView(Uri uri) {
+        Picasso.get().load(uri).into(profilePic);
+    }
+    public void setToastView(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    public void setPdView(String msg){
+        pd.setTitle(msg);
+        pd.show();
+    }
+    public void pdDismissView(){
+        pd.dismiss();
+    }
+}
